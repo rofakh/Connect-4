e235e11a1d615e1309f4a2c8e859cb35b9a590cf
rofakh <rofakhalidelsharkawy@gmail.com>
2025-04-30 11:39:39 +0300

    Update Connect4.py

--- Connect4.py
+++ Connect4.py
@@ -10,3 +10,16 @@ YELLOW = (255,255,0)
 
 ROW_COUNT = 6
 COLUMN_COUNT = 7
+def create_board(): #     6          7
+	board = np.zeros((ROW_COUNT,COLUMN_COUNT))
+	return board
+
+def is_valid_location(board, col):
+	return board[ROW_COUNT-1][col] == 0
+def get_next_open_row(board, col):
+	for r in range(ROW_COUNT):
+		if board[r][col] == 0:
+			return r
+
+def drop_piece(board, row, col, piece):
+	board[row][col] = piece
